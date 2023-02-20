adc8b5346fa1e9464f3093a0ab0465ced54c9090
Marcelo Masahiko Miyake <marcelo.m.miyake@gmail.com>
2023-02-20 07:25:48 -0300

    feat(javascript): #12in23 https://exercism.org/tracks/javascript

---
+++ exercism/javascript/vehicle-purchase/vehicle-purchase.js
@@ -0,0 +1,51 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines whether or not you need a licence to operate a certain kind of vehicle.
+ *
+ * @param {string} kind
+ * @returns {boolean} whether a license is required
+ */
+export function needsLicense(kind) {
+  return kind === 'car' || kind === 'truck';
+}
+
+/**
+ * Helps choosing between two options by recommending the one that
+ * comes first in dictionary order.
+ *
+ * @param {string} option1
+ * @param {string} option2
+ * @returns {string} a sentence of advice which option to choose
+ */
+export function chooseVehicle(option1, option2) {
+  const rest = ' is clearly the better choice.';
+  const cars = ['Bugatti Veyron', 'Ford Pinto', 'Chery EQ', 'Kia Niro Elektro', '2018 Bergamont City', '2020 Gazelle Medeo']
+  for (let car of cars) {
+    if (car === option1 || car === option2) {
+      return car + rest;
+    }
+  }
+
+}
+
+/**
+ * Calculates an estimate for the price of a used vehicle in the dealership
+ * based on the original price and the age of the vehicle.
+ *
+ * @param {number} originalPrice
+ * @param {number} age
+ * @returns {number} expected resell price in the dealership
+ */
+export function calculateResellPrice(originalPrice, age) {
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age <= 10) {
+    return originalPrice * 0.7;
+  }
+  return originalPrice * 0.5;
+}
